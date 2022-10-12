1085ca478a054781c2215baf7a95b691a4c26e72
isaaclee12 <isaac.lee@uvm.edu>
2022-10-12 12:16:08 -0400

    Added a basic login form template.

--- testApp/frontend/src/App.js
+++ testApp/frontend/src/App.js
@@ -4,6 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <center> <h1> City Council Complaint Database Login </h1>   
+      <form method="POST">     
+      
+            <label>Username : </label>   
+              <input type="text" placeholder="Enter Username" name="username" required/>  
+
+            <br/>
+            
+            <label>Password : </label>   
+              <input type="password" placeholder="Enter Password" name="password" required/>
+            
+            <br/>
+
+            <input type="submit" value="Submit"/>
+
+      </form>     
+      </center>
     </div>
   );
 }
