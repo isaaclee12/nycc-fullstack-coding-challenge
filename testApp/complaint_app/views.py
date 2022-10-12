a4b9856ff45c4e3635c3844e735f09e064a126df
isaaclee12 <isaac.lee@uvm.edu>
2022-10-12 13:05:20 -0400

    updating views

--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -5,27 +5,43 @@ from rest_framework.response import Response
 from rest_framework import status
 # Create your views here.
 
+
+# TODO: This is the API part.
+
 class ComplaintViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
   serializer_class = ComplaintSerializer
   def list(self, request):
+    
     # Get all complaints from the user's district
+    # SQL_request("SELECT * FROM tablename WHERE COUNCILMEMBER ID = [Id number from login]")
+
     return Response()
 
 class OpenCasesViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
   def list(self, request):
+
     # Get only the open complaints from the user's district
+    # SQL_request("SELECT * FROM tablename WHERE COUNCILMEMBER ID = [Id number from login] AND OPEN = true")
+
+
     return Response()
 
 class ClosedCasesViewSet(viewsets.ModelViewSet):
   http_method_names = ['get'] 
   def list(self, request):
+
     # Get only complaints that are close from the user's district
+        # SQL_request("SELECT * FROM tablename WHERE COUNCILMEMBER ID = [Id number from login] AND OPEN = false")
+    
     return Response()
     
 class TopComplaintTypeViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
   def list(self, request):
+
     # Get the top 3 complaint types from the user's district
+    # SQL_request("SELECT * FROM tablename WHERE COUNCILMEMBER ID = [Id number from login] AND OPEN = true")
+
     return Response()
