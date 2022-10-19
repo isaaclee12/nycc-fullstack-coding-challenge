2e8386a239634c81a784f8b32ae39c17c47fd1f2
isaaclee12 <isaac.lee@uvm.edu>
2022-10-19 05:41:57 -0400

    Fixed one CORS error on react by setting headers for request... now onto the next error ayayay

--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -1,3 +1,4 @@
+from http.client import HTTPResponse
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,19 +20,29 @@ class ComplaintViewSet(viewsets.ModelViewSet):
     
     # Get all complaints from the user's district, i.e. Read method
     # Get all of the data for complaints via the Complain model (This automatically pulls from the SQL DB via Django's ORM)
-    complaint_list = Complaint.objects.all()
+    # complaint_list = Complaint.objects.all()
 
-    # The serializer here is basically the ORM that pulls the data from SQLite.
-    complaint_serializer = ComplaintSerializer(complaint_list, context={"request": request}, many=True)
+    # # The serializer here is basically the ORM that pulls the data from SQLite.
+    # complaint_serializer = ComplaintSerializer(complaint_list, context={"request": request}, many=True)
 
-    # Send it as a JsonResponse for React to process
-    response = JsonResponse(complaint_serializer.complaint_list, safe=False) # safe = false tells django that this is a valid format
+    # # Send it as a JsonResponse for React to process
+    # response = JsonResponse(complaint_serializer.complaint_list, safe=False) # safe = false tells django that this is a valid format
+
+    # response = JsonResponse(
+    #   {
+    #     "test": 1
+    #   }
+    # )
+
+    response = HTTPResponse("testing")
 
     # Set HTTP Headers for the response to whitelist on CORS and such
-    response["Access-Control-Allow-Origin"] = "http://localhost:3000/complaints"
-    response["Access-Control-Allow-Methods"] = "GET"
-    response["Access-Control-Max-Age"] = "1000"
-    response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+    response["Access-Control-Allow-Origin"] = "http://localhost:3000"
+    # response["Access-Control-Allow-Methods"] = "GET"
+    # response["Access-Control-Max-Age"] = "1000"
+    # response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+
+    print(response)
 
     return response
 
--- testApp/frontend/src/App.js
+++ testApp/frontend/src/App.js
@@ -1,11 +1,123 @@
 // import libraries
-import React from 'react';
+import React, {Component} from 'react';
 import {Route, Routes} from 'react-router-dom';
 
 // Import other components
 import NavBar from './Nav';
-import Complaints from './Complaints';
+// import Complaints from './Complaints';
 import Login from './Login';
+
+
+
+const API_URL = "http://localhost:8000/"
+class Complaints extends Component {
+
+    constructor(props) {
+        super(props);
+
+        //list that will contain complaints from api
+        this.state={
+            complaints:[]
+        }
+    }
+
+    populateData() {
+        fetch(API_URL+'api/complaints',{
+          method: "GET",
+          mode: "cors",
+          headers: {
+            // 'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "http://localhost:5000",
+            // "Access-Control-Allow-Methods": "GET,OPTIONS,POST,PUT"
+          }
+        })
+          .then((response) => response.json()
+          .then((json) => {
+            console.log(json);
+          })
+            //     data=>{
+            //         this.setState({complaints:data});
+            //     }
+            // )       
+        ).catch(
+          (error) => {
+            console.log("Error fetching complaint data:" + error)
+          }
+        ).finally(
+          console.log("bwah")
+        )
+    }
+
+    componentDidMount() {
+        this.populateData();
+    }
+
+
+    render() {
+
+        // // useState here is used to manage the response recieved from the Django API
+        // const [post, setPost] = useState(null);
+
+        // // useEffect runs only once on page load and manages HTTP requests via axios
+        // useEffect(() => {
+        //     axios.get("http://localhost:8000/complaints/").then(
+        //     (response) => {
+        //         setPost(response.data)
+        //     }
+        //     )
+        // }, []) // "[]" here causes a singular run at the first render
+
+        // declare complaints var
+        const {
+            complaints
+        }=this.state;
+
+        return(
+            <div>
+                <h2>This is the complaints list</h2>
+
+                <table>
+                    <thead>
+                        <tr>
+                            {/* TODO: Order is currently mirroring SQLite3 DB, rearrange later to make sense. */}
+                            <th>unique_key</th>
+                            <th>account</th>
+                            <th>opendate</th>
+                            <th>complaint_type</th>
+                            <th>descriptor</th>
+                            <th>zip</th>
+                            <th>borough</th>
+                            <th>city</th>
+                            <th>council_district</th>
+                            {/* <!-- TODO figure out if this is the c.board for the complainer's dist. or the dist. complaint was made --> */}
+                            <th>community_board</th>
+                            <th>closedate</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {complaints.map(c=>
+                            <tr key={c.unique_key}>
+                                <td key={c.unique_key}></td>
+                                <td key={c.account}></td>
+                                <td key={c.opendate}></td>
+                                <td key={c.complaint_type}></td>
+                                <td key={c.descriptor}></td>
+                                <td key={c.zip}></td>
+                                <td key={c.borough}></td>
+                                <td key={c.city}></td>
+                                <td key={c.council_district}></td>
+                                <td key={c.community_board}></td>
+                                <td key={c.closedate}></td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+
 
 function App() {
 
@@ -27,6 +139,8 @@ function App() {
       </Routes>
       
 
+      {/* Testing complaints */}
+      <Complaints/>
 
     </div>
   );
